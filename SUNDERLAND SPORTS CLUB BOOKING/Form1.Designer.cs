602b690b850a3e0078d53839bf6daddd0d56a28e
israel-boop <obanijesufaniran@gmail.com>
2023-04-13 22:58:33 +0100

    steins

--- SUNDERLAND SPORTS CLUB BOOKING/Form1.Designer.cs
+++ SUNDERLAND SPORTS CLUB BOOKING/Form1.Designer.cs
@@ -28,21 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 132);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(449, 12);
+            progressBar1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 156);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SPLASH SCREEN";
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(473, 156);
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SPLASH SCREEN";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
